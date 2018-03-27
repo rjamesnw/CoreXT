dd379ee491a30f2aeb4f2579af0571d513553d86
James <contactme@jameswilkins.net>
2018-03-27 02:39:32 -0400

    Added Expressions.CreateWhereEqualLambda().

--- Source/CoreXT/Utilities/Expressions.cs
+++ Source/CoreXT/Utilities/Expressions.cs
@@ -4,7 +4,9 @@
 // (see more framework monikers here: https://docs.microsoft.com/en-us/nuget/schema/target-frameworks)
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -69,17 +71,58 @@ namespace CoreXT
         /// <param name="entityMemberType"> Type of the entity member. </param>
         /// <param name="constantValue"> The constant value on the right side of the equality test. </param>
         /// <returns> The new lambda equality expression. </returns>
-        public static Expression<Func<TEntity, bool>> CreateWhereEqualLambda<TEntity, TValue>(string entityMemberName, Type entityMemberType, TValue constantValue)
+        public static Expression<Func<TEntity, bool>> CreateWhereEqualLambda<TEntity, TValue>(string entityMemberName, TValue constantValue)
             where TEntity : class
         {
+            var entityType = typeof(TEntity);
+            var entityMemberInfo = entityType.GetProperty(entityMemberName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy)
+                ?? throw new InvalidOperationException($"No public instance property with name '{entityMemberName}' could be found on the entity '{entityType.Name}'.");
+
             ParameterExpression pe = Expression.Parameter(typeof(TEntity), "m");
-            var left = Expression.Property(pe, entityMemberType, entityMemberName);
+            var left = Expression.Property(pe, entityMemberInfo);
             var valueType = typeof(TValue);
+
             Expression right = Expression.Constant(constantValue, valueType);
-            if (valueType != entityMemberType)
-                right = Expression.Convert(right, entityMemberType);
+            if (valueType != entityMemberInfo.PropertyType)
+                right = Expression.Convert(right, entityMemberInfo.PropertyType);
             Expression eq = Expression.Equal(left, right);
+
             var lam = Expression.Lambda<Func<TEntity, bool>>(eq, pe);
+            return lam;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        ///     Dynamically creates a lambda expression in the form "m=>m.MemberName==constantValue" that can be used with LINQ
+        ///     'Where()' filter calls. If the member type and constant value type are different, a conversion expression is
+        ///     inserted as well.
+        /// </summary>
+        /// <typeparam name="TEntity"> Type of the entity. </typeparam>
+        /// <typeparam name="TSource"> Type of the values to filter on. </typeparam>
+        /// <param name="entityMemberName">
+        ///     Name of the entity member to find in the given array of values.
+        /// </param>
+        /// <param name="entityMemberType"> Type of the entity member. </param>
+        /// <param name="values"> The constant value on the right side of the equality test. </param>
+        /// <returns> The new lambda equality expression. </returns>
+        public static Expression<Func<TEntity, bool>> CreateWhereContainsLambda<TEntity, TSource>(string entityMemberName, TSource[] values)
+            where TEntity : class
+        {
+            var entityType = typeof(TEntity);
+            var entityMemberInfo = entityType.GetProperty(entityMemberName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy)
+                ?? throw new InvalidOperationException($"No public instance property with name '{entityMemberName}' could be found on the entity '{entityType.Name}'.");
+
+            var pe = Expression.Parameter(typeof(TEntity), "m");
+            Expression peMember = Expression.Property(pe, entityMemberInfo);
+            var valueType = typeof(TSource);
+            if (valueType != entityMemberInfo.PropertyType)
+                peMember = Expression.Convert(peMember, entityMemberInfo.PropertyType);
+
+            Func<IEnumerable<TSource>, TSource, bool> contains = Enumerable.Contains; // (to get the method info)
+            Expression body = Expression.Call(Expression.Constant(values), contains.Method, peMember);
+
+            var lam = Expression.Lambda<Func<TEntity, bool>>(body, pe);
             return lam;
         }
 
